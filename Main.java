ad80173b72d2ba71daf8d9a63ca14a7165e9d304
Beyza Tasdelen <beyzatasdelen@MacBook-Air-de-Beyza.local>
2019-05-10 19:17:12 +0200

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,18 @@
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.*;
+
+
+public class Main{
+	public static void main(String[] args){
+		try{
+			EspaceVectorielle v1=new EspaceVectorielle("w2v_final3.csv");
+			List<Double> vec1=v1.getVector("mère");
+			List<Double> vec2=v1.getVector("femme");
+			System.out.println(VectorProjet.cosinus(vec1,vec2));
+
+		}catch(IOException e){
+			System.out.println(e);
+		}
+	}
+}
