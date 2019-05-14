bc24f0c9884fa165ab9c2ce33b50a0454ea352b9
Beyza Tasdelen <beyzatasdelen@openroam-prg-og-1-157-60.net.univ-paris-diderot.fr>
2019-05-14 12:23:27 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -8,7 +8,7 @@ public class Main{
 		try{
 			EspaceVectorielle v1=new EspaceVectorielle("w2v_final3.csv");
 			List<Double> vec1=v1.getVector("mère");
-			List<Double> vec2=v1.getVector("femme");
+			List<Double> vec2=v1.getVector("chaise");
 			System.out.println(VectorProjet.cosinus(vec1,vec2));
 
 		}catch(IOException e){
