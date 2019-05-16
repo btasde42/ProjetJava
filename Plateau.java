7e9915d0e8c1b98271fc597f375fc18598fdc669
Clarence <43968207+clarencewang0420@users.noreply.github.com>
2019-05-16 17:38:31 +0200

    Add files via upload

---
+++ Plateau.java
@@ -0,0 +1,42 @@
+
+import java.util.*;
+//c'est un plateau de ce jeu, on le voit comme un tableau de Cases
+public class Plateau {
+	//variables
+	private final Case c1; //parce que dans le plateau il y a 3 types de case, on doit donc faire 3 fois de cases
+	private final Case c2;
+	private final Case c3;
+	private Case [] p;
+	private static int n=40; //n est la taille de ce plateau
+
+	//constucteur, on initialise tous
+	public Plateau() {
+		c1=new Case();
+		c2=new Case();
+		c3=new Case(); 
+		p=new Case[n]; 
+		for(int i=0;i<n;i++){
+			if (i%2==0){
+				p[i]=c2;
+			}else if (i%3==0){
+				p[i]=c3;
+			}else{
+				p[i]=c1;
+			}
+		}
+	}
+
+	//les methodes pour afficher la taille de ce plateau et aussi la plateau
+	public int size(){
+		return n;
+	}
+
+	public void affiche(){
+		for (int i=0;i<this.n;i++){
+			System.out.print(p[i]);
+		}
+		System.out.println();
+	}
+
+
+}
